bb3d90349260efbe9cac02e62009a92bd360e9e6
VoNgocDuc <64114650+VoNgocDuc@users.noreply.github.com>
2022-11-14 01:46:38 +0700

    Add files via upload
    init

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = '';
+const giftUrl = '#';
+const eventName = 'Chúc Mừng Sinh Nhật Em Nhé ❤❤❤';
+const titleCard = '투이 즈엉 생일 축하해';
+const contentCard = 'Anh không có giỏi làm văn cho lắm, thôi thì chỉ dám chúc em tuổi mới hành trình mới có tất cả những điều em muốn đạt được nhé';
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
---
+++ present.js
@@ -0,0 +1,51 @@
+var to = nameGirl;
+var gift_url = giftUrl;
+var gift_image_url = giftImage || giftImageBase64;
+
+var nametag = document.getElementById("nametag");
+var present = document.getElementById("present");
+var presentImage = document.getElementById("present-image");
+var index = 1;
+setInterval(() => {
+  var img = document.getElementById("sidle-image");
+  if (index > 4) {
+    index = 1;
+  }
+  img.setAttribute('src', `img/${index}.jpg`); 
+  index++;
+}, 2500);
+
+function init() {
+  var graphElem = document.querySelector('.present-box > .side.top .to');
+  graphElem.setAttribute('data-before', eventName);
+  document.querySelector('#card .title-card').innerHTML = `💘${titleCard}💘`;
+  document.querySelector('#card .content-card').innerHTML = `Anh không có giỏi làm văn cho lắm, thôi thì chỉ dám chúc em tuổi mới hành trình mới có tất cả những điều em muốn đạt được nhé!!!`;
+
+  var _giftLink,
+    _giftImg;
+
+  if (gift_url) {
+    _giftLink = document.createElement("a");
+    presentImage.appendChild(_giftLink);
+  }
+
+  if (gift_image_url) {
+    _giftImg = document.createElement("img");
+    _giftImg.src = gift_image_url;
+    if (_giftLink) {
+      _giftLink.appendChild(_giftImg);
+    } else {
+      presentImage.appendChild(_giftImg);
+    }
+  }
+
+  present.addEventListener("click", function (e) {
+    present.classList.toggle("open");
+    document.getElementById('card').classList.add('card-show');
+    document.getElementById('sidle-image').classList.add('card-show');
+  }, false);
+ 
+}
+
+init();
+
